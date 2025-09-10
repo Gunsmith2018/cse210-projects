9187cdee95af2caa38416b0039a25f096b35a6f5
Gunsmith2018 <aaronjonathansmith@gmail.com>
2025-09-10 16:45:26 -0500

    creating_new_Journal

---
+++ week02/Journal/Prompts.cs
@@ -0,0 +1,21 @@
+public class Prompts
+{
+    private string[] _prompts = new string[]
+    {
+        "What made you smile today?",
+        "Describe a moment you felt proud.",
+        "What do you want to achieve this week?",
+        "What’s something you’re grateful for right now?",
+        "What’s a challenge you overcame recently?"
+    };
+
+
+
+
+    public Prompts(string[] prompts)
+    {
+        _prompts = prompts;
+    }
+
+
+}
