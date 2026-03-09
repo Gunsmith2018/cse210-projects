38b7f572e39713a2ddc964ecb6a08d3206aa4c20
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-03-08 20:45:41 -0500

    created and edited exercise 1,2,3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,29 +5,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
 
+        // variables used for grading
         int a = 90;
         int b = 80;
         int c = 70;
         int d = 60;
-        int f = 50;
+        
+        int percent; // This will be used for converting the string into an int
 
-        Console.Write("Please tell me what percent you got on your grade: ");
-        string input = Console.ReadLine();
-        int percent;
-        if (int.TryParse(input, out percent) )
+        while (true) // While loop used for looping the user untill a valid number is inputed
         {
-            if (percent >= a)
-            Console.WriteLine("Congragulations. That is an A: ");
-            if (percent >= b)
-            Console.WriteLine("Congragulations. That is an b: ");
-            if (percent >= c)
-            Console.WriteLine("Congragulations. That is an c: ");
-            if (percent >= d)
-            Console.WriteLine("Unfortantly, that is a failing: D");
-            if (percent >= f)
-            Console.WriteLine("Unfortanatly, that is failing: F");
+            Console.WriteLine("Please tell me what percent you got on your grade or type exit to leave: ");
+            string input = Console.ReadLine(); // Saving the user input into a string
+
+            if (input.ToLower() == "exit")
+            {
+                Console.WriteLine("Thank you for using a simple grading system.");
+                break; // used for leaving a while loop
+            }
+
+            if (int.TryParse(input, out percent)) // converting the user's unput into a usable in for "percent"
+            {
+                    if (percent >= a) // Asking for what grade the user got. Currently set up for only BASE grades no plus or minus yet. 
+                    {
+                        Console.WriteLine("Congragulations. That is an A: ");
+                    }
+                    else if (percent >= b)
+                    {
+                        Console.WriteLine("Congragulations. That is an b: ");
+                    }
+                    else if (percent >= c)
+                    {
+                        Console.WriteLine("Congragulations. That is an c: ");
+                    }
+                    else if (percent >= d)
+                    {
+                        Console.WriteLine("Unfortantly, that is a failing: D");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Unfortanatly, that is failing: F");
+                    }
+                break; // used for leaving a while loop
+            }
+            else // Crash out condition for when the User doesn't input a valid number
+            {
+                Console.WriteLine("Sorry, you have entered a invalid number. Please enter a valid number: ");
+            }
         }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Random Number Generator
+        Random  randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        for (int guess = -1; guess != number;) // Checks that the User input isn't equal to the "magic" number
+        {
+            Console.Write("Guess a number between 1 and 100: ");
+            Console.WriteLine("===================================="); // spacer
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out guess)) // converts string into a usable int format
+            {
+                if (guess > number)
+                {
+                    Console.WriteLine("Your guess is too HIGH, try LOWER: ");
+                    Console.WriteLine("===================================="); //spacer
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Your guess is too LOW, try Higher: ");
+                    Console.WriteLine("===================================="); // spacer
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again with a valid number.");
+                Console.WriteLine("===================================="); // spacer
+            }
+        }
+        Console.WriteLine("Congratulations! You got it.");
+        Console.WriteLine("===================================="); // spacer
     }
 }
