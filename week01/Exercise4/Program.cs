17d29e7f9f11adfb3e3a0bd97b0ab76ce7cb665b
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-03-10 15:01:41 -0500

    created exercise 5 and added in notes

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0) // runs the program as long as the user doesn't type 0
+        {
+            Console.Write("Please enter some numbers excluding 0, unless you want to exit: ");
+            string user = Console.ReadLine();
+
+            if (int.TryParse(user, out userNumber)) // switches the suer input to a int rather than a string
+            {
+                if (userNumber != 0) // Adds to "numbers" string excluding 0
+                {
+                    numbers.Add(userNumber);
+                }
+            }
+
+            int sum = 0;
+
+            foreach (int number in numbers) // adding up the numbers in the string. 
+            {
+                sum += number;
+            }
+
+            Console.WriteLine($"The sum is: {sum}"); // displaying the sum of the string
+        }
+
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    // Variables and string creation
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResults(userName, squaredNumber);
+
+        void DisplayWelcomeMessage() // Welcome Function
+        {
+            Console.WriteLine("Wellcome to a simple squaring program.");
+
+        }
+
+        static string PromptUserName() // Prompting the user for their name function
+        {
+            Console.Write("Please tell me your name");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber() // Prompting the user for a number function
+        {
+            Console.WriteLine("Please give me a number to be squared");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+            
+        static int SquareNumber(int number) // Math function set up for squaring the user's number
+        {
+            int square = number * number;
+            return square;
+        }
+        
+        static void DisplayResults(string name, int square) // Displaying the User's name and number Function
+        {
+            Console.WriteLine($"{name} the square of your number is {square}");
+        }
     }
 }
