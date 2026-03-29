428f1c38ae45b0c1ca82f88e744e4fe8244012ef
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-03-28 20:43:29 -0500

    Creating youtube code

---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,21 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    string Title(string Title)
+    {
+        foreach ()
+        {
+
+        }
+    }
+}
