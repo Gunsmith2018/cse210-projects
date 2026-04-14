ee36ab5e7bb54e258caba6876d088ac3653e21f3
Gunsmith2018 <aaronjonathansmith@gmail.com>
2026-04-13 20:49:37 -0500

    added code to program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>()
+        {
+            new Running("03 Nov 2022", 30, 3),      // 3 miles (or km)
+            new Cycling("03 Nov 2022", 30, 10),     // 10 mph (or kph)
+            new Swimming("03 Nov 2022", 30, 40)     // 40 laps
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
